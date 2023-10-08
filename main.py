1ff8f1c603dee36ecff77f41a6cefcc6da8ffc9d
Phanith LIM <limphanith.office@gmail.com>
2023-10-08 23:50:47 +0700

    Deploy 1.2

--- main.py
+++ main.py
@@ -37,4 +37,4 @@ app.include_router(auth_router)
 app.include_router(user_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8080)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
